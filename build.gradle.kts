a891333930ebf77c2c93a1c5d2033d1cafd06ecc
Bjørn Faaberg <bjof@nav.no>
2025-09-10 12:22:46 +0200

    Adding import of jackson datatype jsr310

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages")
     implementation(libs.koin.ktor)
     implementation(libs.koin.logger)
+    implementation(libs.jackson.datatype.jsr310)
     // Database
     implementation(libs.bundles.database)
     // Metrics and Prometheus

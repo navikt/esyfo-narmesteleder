84b58224ce8f1739a4137971823b6f0d1430b923
Bjørn Faaberg <bjof@nav.no>
2025-09-11 14:02:14 +0200

    Move FakeSykemeldingNLKafkaProducer to its own file

--- src/main/kotlin/no/nav/syfo/narmesteleder/kafka/SykemeldingNLKafkaProducer.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/kafka/SykemeldingNLKafkaProducer.kt
@@ -1,6 +1,5 @@
 package no.nav.syfo.narmesteleder.kafka
 
-import java.security.MessageDigest
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import no.nav.syfo.narmesteleder.kafka.model.INlResponseKafkaMessage
@@ -9,7 +8,6 @@ import no.nav.syfo.narmesteleder.kafka.model.NlAvbruddResponseKafkaMessage
 import no.nav.syfo.narmesteleder.kafka.model.NlAvbrutt
 import no.nav.syfo.narmesteleder.kafka.model.NlRelationResponseKafkaMessage
 import no.nav.syfo.narmesteleder.kafka.model.NlResponse
-import no.nav.syfo.util.logger
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 
@@ -45,30 +43,5 @@ class SykemeldingNLKafkaProducer(private val producer: KafkaProducer<String, INl
     companion object {
         const val SYKEMELDING_NL_TOPIC = "teamsykmelding.syfo-narmesteleder"
         const val PRODUCER_SOURCE = "syfo-narmesteleder"
-    }
-}
-
-class FakeSykemeldingNLKafkaProducer : ISykemeldingNLKafkaProducer {
-    val logger = logger()
-    override fun sendSykemeldingNLRelasjon(sykemeldingNL: NlResponse) {
-        logger.info("FakeSykemeldingNLKafkaProducer sendSykemeldingNLRelasjon to orgnummer: ${sykemeldingNL.orgnummer}")
-        logger.info(sykemeldingNL.hashFnrFields().toString())
-    }
-
-    override fun sendSykemeldingNLBrudd(nlAvbrutt: NlAvbrutt) {
-        logger.info("FakeSykemeldingNLKafkaProducer sendSykemeldingNLBrudd to orgnummer: ${nlAvbrutt.orgnummer}")
-        logger.info(nlAvbrutt.copy(sykmeldtFnr = nlAvbrutt.sykmeldtFnr.sha256()).toString())
-    }
-
-    fun NlResponse.hashFnrFields(): NlResponse {
-        return this.copy(
-            leder = this.leder.copy(fnr = this.leder.fnr.sha256()),
-            sykmeldt = this.sykmeldt.copy(fnr = this.sykmeldt.fnr.sha256())
-        )
-    }
-
-    private fun String.sha256(): String {
-        val bytes = MessageDigest.getInstance("SHA-256").digest(this.toByteArray())
-        return bytes.joinToString("") { "%02x".format(it) }
     }
 }

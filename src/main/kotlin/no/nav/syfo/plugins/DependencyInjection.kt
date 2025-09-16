2785df8dc982ef6f07b6f0962ba5efb4d8242db3
Bjørn Faaberg <bjof@nav.no>
2025-09-16 09:10:47 +0200

    Fix TexasClient

--- src/main/kotlin/no/nav/syfo/plugins/DependencyInjection.kt
+++ src/main/kotlin/no/nav/syfo/plugins/DependencyInjection.kt
@@ -19,6 +19,7 @@ import no.nav.syfo.narmesteleder.service.NarmestelederKafkaService
 import no.nav.syfo.pdl.PdlService
 import no.nav.syfo.pdl.client.FakePdlClient
 import no.nav.syfo.pdl.client.PdlClient
+import no.nav.syfo.texas.client.TexasHttpClient
 import no.nav.syfo.util.httpClientDefault
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.common.serialization.StringSerializer
@@ -70,17 +71,6 @@ private fun databaseModule() = module {
 
 private fun servicesModule() = module {
     single { TexasHttpClient(client = get(), environment = env().texas) }
-    single {
-        if (isLocalEnv()) FakePdlClient() else PdlClient(
-            httpClient = get(),
-            pdlBaseUrl = env().clientProperties.pdlBaseUrl,
-            texasHttpClient = get(),
-            scope = env().clientProperties.pdlScope
-        )
-    }
-    single {
-        PdlService(get())
-    }
     single {
         if (isLocalEnv()) FakePdlClient() else PdlClient(
             httpClient = get(),

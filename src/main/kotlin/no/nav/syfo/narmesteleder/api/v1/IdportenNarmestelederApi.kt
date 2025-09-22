9d404aaef410441e965d4797fade0a426e37bc29
Bjørn Faaberg <bjof@nav.no>
2025-09-22 15:00:48 +0200

    Skip plugin to get BrukerPrincipal

--- src/main/kotlin/no/nav/syfo/narmesteleder/api/v1/IdportenNarmestelederApi.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/api/v1/IdportenNarmestelederApi.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.narmesteleder.api.v1
 
 import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.JsonConvertException
+import io.ktor.server.auth.principal
 import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
@@ -9,6 +10,8 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import no.nav.syfo.altinntilganger.AltinnTilgangerService
+import no.nav.syfo.application.auth.BrukerPrincipal
+import no.nav.syfo.application.exceptions.UnauthorizedException
 import no.nav.syfo.narmesteleder.kafka.model.NlResponseSource
 import no.nav.syfo.narmesteleder.service.NarmestelederKafkaService
 
@@ -23,7 +26,9 @@ fun Route.registerIdportenNarmestelederApiV1(
             } catch (e: JsonConvertException) {
                 throw BadRequestException("Invalid payload in request: ${e.message}", e)
             }
-            val bruker = call.attributes[BRUKER_PRINCIPAL]
+//            val bruker = call.attributes[BRUKER_PRINCIPAL]
+            val bruker = call.principal<BrukerPrincipal>()
+                ?: throw UnauthorizedException("No user principal found in request")
             altinnTilgangerService.validateTilgangToOrganisasjon(bruker, nlRelasjon.organisasjonsnummer)
             narmestelederKafkaService.sendNarmesteLederRelation(nlRelasjon, NlResponseSource.LPS)
             call.respond(HttpStatusCode.Accepted)
@@ -31,8 +36,13 @@ fun Route.registerIdportenNarmestelederApiV1(
     }
 
     route("/narmesteleder/avkreft") {
+//        install(AddBrukerPrincipalPlugin) {
+//            this.texasHttpClient = texasHttpClient
+//        }
         post() {
-            val bruker = call.attributes[BRUKER_PRINCIPAL]
+            val bruker = call.principal<BrukerPrincipal>()
+                ?: throw UnauthorizedException("No user principal found in request")
+//            val bruker = call.attributes[BRUKER_PRINCIPAL]
             val avkreft = try {
                 call.receive<NarmestelederRelasjonAvkreft>()
             } catch (e: JsonConvertException) {

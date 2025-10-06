604833eb7a5f2854afe3f9165f0821a3d81ea603
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-10-06 13:30:04 +0200

    Debug with logging

--- src/main/kotlin/no/nav/syfo/aareg/AaregService.kt
+++ src/main/kotlin/no/nav/syfo/aareg/AaregService.kt
@@ -1,8 +1,10 @@
 package no.nav.syfo.aareg
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.syfo.aareg.client.AaregClientException
 import no.nav.syfo.aareg.client.IAaregClient
 import no.nav.syfo.application.exception.ApiErrorException
+import no.nav.syfo.util.logger
 
 class AaregService(private val arbeidsforholdOversiktClient: IAaregClient) {
     suspend fun findOrgNumbersByPersonIdent(
@@ -18,15 +20,19 @@ class AaregService(private val arbeidsforholdOversiktClient: IAaregClient) {
                 e
             )
         }
+        logger().info(ObjectMapper().writeValueAsString(arbeidsforholdOversikt))
 
         // Ansettelsforhold gitt ved organisasjonsnummer i over- og underenhet
-        return arbeidsforholdOversikt.arbeidsforholdoversikter.mapNotNull { arbeidsforhold ->
+        val map = arbeidsforholdOversikt.arbeidsforholdoversikter.mapNotNull { arbeidsforhold ->
             val orgnummer = arbeidsforhold.arbeidssted.getOrgnummer()
             val juridiskOrgnummer = arbeidsforhold.opplysningspliktig.getJuridiskOrgnummer()
+            logger().info("Arbeidsforhold for person: $orgnummer, juridisk: $juridiskOrgnummer")
             if (orgnummer != null && juridiskOrgnummer != null) {
                 orgnummer to juridiskOrgnummer
             } else null
         }
             .toMap()
+        logger().info(ObjectMapper().writeValueAsString(map))
+        return map
     }
 }

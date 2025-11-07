828fcae5df2d862b2f029517de1a6126559e51f4
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2025-11-07 07:54:20 +0100

    Add test for the outgoing patch call

--- src/main/kotlin/no/nav/syfo/dialogporten/client/DialogportenClient.kt
+++ src/main/kotlin/no/nav/syfo/dialogporten/client/DialogportenClient.kt
@@ -1,6 +1,8 @@
 package no.nav.syfo.dialogporten.client
 
+import com.fasterxml.jackson.annotation.JsonValue
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.bearerAuth
@@ -12,19 +14,20 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
+import io.ktor.http.content.TextContent
+import io.ktor.http.contentType
 import io.ktor.http.parameters
-import no.nav.syfo.dialogporten.domain.Dialog
-import no.nav.syfo.texas.client.TexasHttpClient
-import no.nav.syfo.util.logger
-import java.util.UUID
+import java.util.*
 import no.nav.syfo.dialogporten.domain.Content
 import no.nav.syfo.dialogporten.domain.ContentValue
 import no.nav.syfo.dialogporten.domain.ContentValueItem
+import no.nav.syfo.dialogporten.domain.Dialog
 import no.nav.syfo.dialogporten.domain.DialogStatus
 import no.nav.syfo.dialogporten.domain.ExtendedDialog
+import no.nav.syfo.texas.client.TexasHttpClient
+import no.nav.syfo.util.JSON_PATCH_CONTENT_TYPE
+import no.nav.syfo.util.logger
 
-private const val DIALOG_ID_PARAM_NAME = "externalReference"
-private const val JSON_PATCH_CONTENT_TYPE = "application/json-patch+json"
 
 interface IDialogportenClient {
     suspend fun createDialog(dialog: Dialog): UUID
@@ -34,6 +37,7 @@ interface IDialogportenClient {
 }
 
 private const val DIGDIR_TARGET_SCOPE = "digdir:dialogporten.serviceprovider"
+private const val DIALOG_ID_PARAM_NAME = "externalReference"
 
 class DialogportenClient(
     private val baseUrl: String,
@@ -74,23 +78,23 @@ class DialogportenClient(
 
         runCatching {
             httpClient
-                .patch("dialogportenUrl/$dialogId") {
-                    header(HttpHeaders.ContentType, JSON_PATCH_CONTENT_TYPE)
+                .patch("$dialogportenUrl/$dialogId") {
                     header(HttpHeaders.Accept, ContentType.Application.Json)
                     header(HttpHeaders.IfMatch, revisionNumber.toString())
+                    contentType(JSON_PATCH_CONTENT_TYPE)
                     bearerAuth(token)
                     setBody(
                         listOf(
-                            mapOf(
-                                "op" to "replace",
-                                "path" to "/status",
-                                "value" to dialogStatus.name
+                            DialogportenPatch(
+                                DialogportenPatch.OPERATION.REPLACE,
+                                "/status",
+                                dialogStatus.name
                             )
                         )
                     )
                 }
         }.onFailure { e ->
-            logger.error("Feil ved kall til Dialogporten for å opprette dialog", e)
+            logger.error("Feil ved kall til Dialogporten for å oppdatere dialog", e)
             throw DialogportenClientException(e.message ?: "Feil ved kall til Dialogporten")
         }
     }
@@ -137,6 +141,21 @@ class DialogportenClient(
         val texasResponse = texasHttpClient.systemToken("maskinporten", DIGDIR_TARGET_SCOPE)
         return altinnExchange(texasResponse.accessToken)
     }
+
+    internal class DialogportenPatch(
+        val op: OPERATION,
+        val path: String,
+        val value: String,
+    ) {
+        enum class OPERATION(val jsonValue: String) {
+            REPLACE("Replace"),
+            ADD("Add"),
+            REMOVE("Remove");
+
+            @JsonValue
+            fun toJson() = jsonValue
+        }
+    }
 }
 
 class FakeDialogportenClient : IDialogportenClient {
@@ -173,4 +192,5 @@ class FakeDialogportenClient : IDialogportenClient {
             serviceResource = "service:resource",
             transmissions = listOf(),
         )
+
 }
---
+++ src/test/kotlin/no/nav/syfo/dialogporten/client/DialogportenClientTest.kt
@@ -0,0 +1,91 @@
+package no.nav.syfo.dialogporten.client
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import createMockToken
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.arbitrary.TypeReference
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.engine.mock.toByteArray
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.fullPath
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.spyk
+import java.util.*
+import no.nav.syfo.util.JSON_PATCH_CONTENT_TYPE
+import no.nav.syfo.dialogporten.domain.DialogStatus
+import no.nav.syfo.texas.client.TexasHttpClient
+import no.nav.syfo.texas.client.TexasResponse
+import no.nav.syfo.util.httpClientDefault
+
+
+class DialogportenClientTest : DescribeSpec({
+    describe("Test PATCH status in dialogporten") {
+        context("Should follow RFC specs when sending the PATCH request") {
+            val httpClientWithAssertions = httpClientDefault(HttpClient(engine = MockEngine { request ->
+                when (request.method) {
+                    HttpMethod.Patch -> {
+                        // Noe quirk med MockEngine gjør at headeren tydeligvis havner her
+                        request.body.contentType shouldBe JSON_PATCH_CONTENT_TYPE
+                        val patchValues: List<DialogportenClient.DialogportenPatch> =
+                            jacksonObjectMapper().readValue(request.body.toByteArray())
+
+                        patchValues.first().path shouldBe "/status"
+                        patchValues.first().value shouldBe DialogStatus.Completed.name
+                        patchValues.first().op shouldBe DialogportenClient.DialogportenPatch.OPERATION.REPLACE
+                        patchValues.first().op.jsonValue shouldBe "Replace"
+
+                        respond(
+                            content = "",
+                            status = HttpStatusCode.NoContent,
+                        )
+                    }
+
+                    HttpMethod.Get if request.url.fullPath.contains("exchange") -> {
+                        respond(
+                            content = createMockToken("12345678901")
+                        )
+                    }
+
+                    else -> error("Unhandled request ${request.url.fullPath}")
+                }
+            }))
+            val mockTexasHttpClient = mockk<TexasHttpClient>(relaxed = true)
+            val dialogportenClient = spyk(
+                DialogportenClient(
+                    baseUrl = "http://localhost:8080",
+                    httpClient = httpClientWithAssertions,
+                    texasHttpClient = mockTexasHttpClient
+                )
+            )
+            it("Should send a patch to Dialogporten with correct headers and body") {
+                val dialogId = UUID.randomUUID()
+                coEvery {
+                    mockTexasHttpClient.systemToken(any(), any())
+                } returns TexasResponse(
+                    "token", 111, "tokenType"
+                )
+                dialogportenClient.updateDialogStatus(
+                    dialogId = dialogId,
+                    revisionNumber = UUID.randomUUID(),
+                    dialogStatus = DialogStatus.Completed,
+                )
+
+                coVerify {
+                    dialogportenClient.updateDialogStatus(
+                        eq(dialogId),
+                        any(),
+                        DialogStatus.Completed
+                    )
+                }
+            }
+        }
+    }
+})

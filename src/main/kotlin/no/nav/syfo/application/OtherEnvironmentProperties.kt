58eb4e1098f5ff15bd03bf573502f9851c6e5d84
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-11-10 07:56:24 +0100

    Activate behov creation (#70)
    - **Activate writing to nl_bohov on leesah messages**
    - **Activate sending kafka message on received linemanager**

--- src/main/kotlin/no/nav/syfo/application/OtherEnvironmentProperties.kt
+++ src/main/kotlin/no/nav/syfo/application/OtherEnvironmentProperties.kt
@@ -4,6 +4,7 @@ data class OtherEnvironmentProperties(
     val electorPath: String,
     val frontendBaseUrl: String,
     val publicIngressUrl: String,
+    val persistLeesahNlBehov: Boolean,
 ) {
     companion object {
         fun createFromEnvVars() =
@@ -11,12 +12,14 @@ data class OtherEnvironmentProperties(
                 electorPath = getEnvVar("ELECTOR_PATH"),
                 frontendBaseUrl = getEnvVar("FRONTEND_BASE_URL"),
                 publicIngressUrl = getEnvVar("PUBLIC_INGRESS_URL"),
+                persistLeesahNlBehov = true
             )
         fun createForLocal() =
             OtherEnvironmentProperties(
                 electorPath = "esyfo-narmesteleder",
                 frontendBaseUrl = "http://localhost:3000",
                 publicIngressUrl = "http://localhost:8080",
+                persistLeesahNlBehov = getEnvVar("PERSIST_LEESAH_NL_BEHOV", "false").toBoolean(),
             )
     }
 }

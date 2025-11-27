c4f13508c69c7e4fdbd5dbd62e62a0c05794e5e7
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-11-27 10:23:24 +0100

    Dispatchers.IO for background tasks

--- src/main/kotlin/no/nav/syfo/plugins/ConfigureTasks.kt
+++ src/main/kotlin/no/nav/syfo/plugins/ConfigureTasks.kt
@@ -2,6 +2,8 @@ package no.nav.syfo.plugins
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationStopping
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.Dispatchers
 import kotlin.getValue
 import kotlinx.coroutines.launch
 import no.nav.syfo.altinn.dialogporten.task.SendDialogTask
@@ -12,13 +14,14 @@ fun Application.configureBackgroundTasks() {
     val sendDialogTask by inject<SendDialogTask>()
     val updateDialogTast by inject<UpdateDialogTask>()
 
-    val sendDialogTaskJob = launch { sendDialogTask.runTask() }
-    monitor.subscribe(ApplicationStopping) {
-        sendDialogTaskJob.cancel()
+    val sendDialogTaskJob = launch(Dispatchers.IO + CoroutineName("send-dialog-task")) {
+        sendDialogTask.runTask()
     }
-
-    val updateDialogTaskJob = launch { updateDialogTast.runSetCompletedTask() }
+    val updateDialogTaskJob = launch(Dispatchers.IO + CoroutineName("update-dialog-task")) {
+        updateDialogTast.runSetCompletedTask()
+    }
     monitor.subscribe(ApplicationStopping) {
         updateDialogTaskJob.cancel()
+        sendDialogTaskJob.cancel()
     }
 }

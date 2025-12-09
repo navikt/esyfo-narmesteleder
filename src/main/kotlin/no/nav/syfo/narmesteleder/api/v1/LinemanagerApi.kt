44118f2ba20b7a86eef582656991c648e0c6e13c
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-12-09 10:27:36 +0100

    Moving RoutingCall functions to get named parameters for collection endpoint

--- src/main/kotlin/no/nav/syfo/narmesteleder/api/v1/LinemanagerApi.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/api/v1/LinemanagerApi.kt
@@ -1,24 +1,12 @@
 package no.nav.syfo.narmesteleder.api.v1
 
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.auth.authentication
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.RoutingCall
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
 import io.ktor.server.routing.route
-import java.time.Instant
-import java.time.format.DateTimeParseException
-import no.nav.syfo.application.api.ErrorType
-import no.nav.syfo.application.auth.JwtIssuer
-import no.nav.syfo.application.auth.SystemPrincipal
-import no.nav.syfo.application.auth.Principal
-import no.nav.syfo.application.auth.TOKEN_ISSUER
-import no.nav.syfo.application.auth.UserPrincipal
-import no.nav.syfo.application.exception.ApiErrorException
-import no.nav.syfo.application.exceptions.UnauthorizedException
 import no.nav.syfo.narmesteleder.domain.Linemanager
 import no.nav.syfo.narmesteleder.domain.LinemanagerRequiremenCollection
 import no.nav.syfo.narmesteleder.domain.LinemanagerRevoke
@@ -27,7 +15,6 @@ import no.nav.syfo.narmesteleder.domain.PageInfo
 import no.nav.syfo.narmesteleder.kafka.model.NlResponseSource
 import no.nav.syfo.narmesteleder.service.NarmestelederKafkaService
 import no.nav.syfo.narmesteleder.service.ValidationService
-import no.nav.syfo.narmesteleder.service.validateNarmesteLeder
 import no.nav.syfo.texas.MaskinportenAndTokenXTokenAuthPlugin
 import no.nav.syfo.texas.client.TexasHttpClient
 
@@ -122,38 +109,3 @@ fun Route.registerLinemanagerApiV1(
         }
     }
 }
-
-fun RoutingCall.getCreatedAfter(): Instant {
-    val createdAfter = getQueryParameter("createdAfter")
-    try {
-        return Instant.parse(createdAfter)
-    } catch (e: DateTimeParseException) {
-        throw ApiErrorException.BadRequestException(
-            "Invalid date format for createdAfter parameter. Expected ISO-8601 format.",
-            type = ErrorType.BAD_REQUEST
-        )
-    }
-}
-
-fun RoutingCall.getPageSize(): Int =
-    this.queryParameters["pageSize"]?.let {
-        it.toIntOrNull().let { pageSize ->
-            when (it.toIntOrNull()) {
-                in 1..LinemanagerRequiremenCollection.DEFAULT_PAGE_SIZE -> pageSize
-                else -> LinemanagerRequiremenCollection.DEFAULT_PAGE_SIZE
-            }
-        }
-    } ?: LinemanagerRequiremenCollection.DEFAULT_PAGE_SIZE
-
-fun RoutingCall.getMyPrincipal(): Principal =
-    when (attributes[TOKEN_ISSUER]) {
-        JwtIssuer.MASKINPORTEN -> {
-            authentication.principal<SystemPrincipal>() ?: throw UnauthorizedException()
-        }
-
-        JwtIssuer.TOKEN_X -> {
-            authentication.principal<UserPrincipal>() ?: throw UnauthorizedException()
-        }
-
-        else -> throw UnauthorizedException()
-    }

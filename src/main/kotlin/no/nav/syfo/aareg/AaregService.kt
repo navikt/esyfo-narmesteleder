b1b093ad93633da0b5435067fd64cd59a60ed4df
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-01-06 14:58:49 +0100

    Add logging of type for arbeidssted and opplysningspliktig type when we do not find both orgnumbers (#144)

--- src/main/kotlin/no/nav/syfo/aareg/AaregService.kt
+++ src/main/kotlin/no/nav/syfo/aareg/AaregService.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.aareg
 import no.nav.syfo.aareg.client.AaregClientException
 import no.nav.syfo.aareg.client.IAaregClient
 import no.nav.syfo.application.exception.ApiErrorException
+import no.nav.syfo.util.logger
 
 class AaregService(private val arbeidsforholdOversiktClient: IAaregClient) {
     suspend fun findOrgNumbersByPersonIdent(
@@ -25,8 +26,16 @@ class AaregService(private val arbeidsforholdOversiktClient: IAaregClient) {
             val juridiskOrgnummer = arbeidsforhold.opplysningspliktig.getJuridiskOrgnummer()
             if (orgnummer != null && juridiskOrgnummer != null) {
                 orgnummer to juridiskOrgnummer
-            } else null
+            } else {
+                logger.warn(
+                    "Could not find arbeidsforhold: orgnummer: $orgnummer, type: ${arbeidsforhold.arbeidssted.type}, juridiskOrgnummer: $juridiskOrgnummer, type: ${arbeidsforhold.opplysningspliktig.type}"
+                )
+                null
+            }
         }
             .toMap()
+    }
+    companion object {
+        private val logger = logger()
     }
 }

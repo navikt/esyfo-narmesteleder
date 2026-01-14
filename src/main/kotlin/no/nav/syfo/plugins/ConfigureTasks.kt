6dfda85e7be5886440814c1533996ba35145da79
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2026-01-14 08:48:18 +0100

    Manually start task jobs on ApplicationStarted to check if they will have an initial run. Leader might not be set with current setup. Something is preventing the maintenance task to run

--- src/main/kotlin/no/nav/syfo/plugins/ConfigureTasks.kt
+++ src/main/kotlin/no/nav/syfo/plugins/ConfigureTasks.kt
@@ -1,8 +1,11 @@
 package no.nav.syfo.plugins
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStarted
 import io.ktor.server.application.ApplicationStopping
 import kotlin.getValue
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import no.nav.syfo.altinn.dialogporten.task.SendDialogTask
 import no.nav.syfo.altinn.dialogporten.task.UpdateDialogTask
@@ -14,23 +17,32 @@ import org.koin.ktor.ext.inject
 fun Application.configureBackgroundTasks() {
     val logger = logger()
     val environment by inject<Environment>()
-    if (!environment.otherProperties.isDialogportenBackgroundTaskEnabled) {
-        logger.info("Integration with Dialogporten is not enabled. Skipping background tasks")
-        return
-    }
-    logger.info("Integration with Dialogporten is enabled. Configuring background tasks")
 
     val sendDialogTask by inject<SendDialogTask>()
     val updateDialogTast by inject<UpdateDialogTask>()
     val behovMaintenanceTask by inject<BehovMaintenanceTask>()
 
-    val behovMaintenanceTaskJob = launch { behovMaintenanceTask.runTask() }
-    val sendDialogTaskJob = launch { sendDialogTask.runTask() }
-    val updateDialogTaskJob = launch { updateDialogTast.runTask() }
+    val behovMaintenanceTaskJob = launch(start = CoroutineStart.LAZY) { behovMaintenanceTask.runTask() }
+    val sendDialogTaskJob = launch(start = CoroutineStart.LAZY) { sendDialogTask.runTask() }
+    val updateDialogTaskJob = launch(start = CoroutineStart.LAZY) { updateDialogTast.runTask() }
 
+    monitor.subscribe(ApplicationStarted) {
+        behovMaintenanceTaskJob.start()
+
+        if (!environment.otherProperties.isDialogportenBackgroundTaskEnabled) {
+            logger.info("Integration with Dialogporten is not enabled. Skipping background tasks")
+            return@subscribe
+        }
+        logger.info("Integration with Dialogporten is enabled. Configuring background tasks")
+        sendDialogTaskJob.start()
+        updateDialogTaskJob.start()
+    }
     monitor.subscribe(ApplicationStopping) {
+        behovMaintenanceTaskJob.cancel()
+
+        if (!environment.otherProperties.isDialogportenBackgroundTaskEnabled)
+            return@subscribe
         sendDialogTaskJob.cancel()
         updateDialogTaskJob.cancel()
-        behovMaintenanceTaskJob.cancel()
     }
 }

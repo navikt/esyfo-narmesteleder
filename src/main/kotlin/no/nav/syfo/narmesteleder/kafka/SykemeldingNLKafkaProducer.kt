8ecc0ce687ce98180e76123c728d1e6bc2fd107d
Malaz Alkoj <malaz.alkoj@nav.no>
2026-01-26 11:30:13 +0100

    More metrics (#164)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main/kotlin/no/nav/syfo/narmesteleder/kafka/SykemeldingNLKafkaProducer.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/kafka/SykemeldingNLKafkaProducer.kt
@@ -1,5 +1,11 @@
 package no.nav.syfo.narmesteleder.kafka
 
+import no.nav.syfo.narmesteleder.api.v1.COUNT_FAILED_ASSIGN_LINEMANAGER_FROM_EMPTY_FORM_BY_LPS
+import no.nav.syfo.narmesteleder.api.v1.COUNT_FAILED_ASSIGN_LINEMANAGER_FROM_EMPTY_FORM_BY_PERSONNEL_MANAGER
+import no.nav.syfo.narmesteleder.api.v1.COUNT_FAILED_REVOKE_LINEMANAGER_FROM_EMPTY_FORM_BY_LPS
+import no.nav.syfo.narmesteleder.api.v1.COUNT_FAILED_REVOKE_LINEMANAGER_FROM_EMPTY_FORM_BY_PERSONNEL_MANAGER
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
 import no.nav.syfo.narmesteleder.kafka.model.INlResponseKafkaMessage
 import no.nav.syfo.narmesteleder.kafka.model.KafkaMetadata
 import no.nav.syfo.narmesteleder.kafka.model.NlAvbruddResponseKafkaMessage
@@ -10,15 +16,14 @@ import no.nav.syfo.narmesteleder.kafka.model.NlResponseSource
 import no.nav.syfo.util.logger
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
 
 interface ISykemeldingNLKafkaProducer {
     fun sendSykmeldingNLRelasjon(sykmeldingNL: NlResponse, source: NlResponseSource)
     fun sendSykmldingNLBrudd(nlAvbrutt: NlAvbrutt, source: NlResponseSource)
 }
 
-class SykemeldingNLKafkaProducer(private val producer: KafkaProducer<String, INlResponseKafkaMessage>) : ISykemeldingNLKafkaProducer {
+class SykemeldingNLKafkaProducer(private val producer: KafkaProducer<String, INlResponseKafkaMessage>) :
+    ISykemeldingNLKafkaProducer {
     override fun sendSykmeldingNLRelasjon(sykmeldingNL: NlResponse, source: NlResponseSource) {
         val kafkaMessage =
             NlRelationResponseKafkaMessage(
@@ -28,6 +33,16 @@ class SykemeldingNLKafkaProducer(private val producer: KafkaProducer<String, INl
         try {
             producer.send(ProducerRecord(SYKEMELDING_NL_TOPIC, sykmeldingNL.orgnummer, kafkaMessage)).get()
         } catch (ex: Exception) {
+            when(source){
+                NlResponseSource.LPS -> {
+                    COUNT_FAILED_ASSIGN_LINEMANAGER_FROM_EMPTY_FORM_BY_LPS.increment()
+                }
+                NlResponseSource.PERSONALLEDER -> {
+                    COUNT_FAILED_ASSIGN_LINEMANAGER_FROM_EMPTY_FORM_BY_PERSONNEL_MANAGER.increment()
+                }
+                else -> {
+                }
+            }
             logger.error("Exception was thrown when attempting to send NlRelasjon: ${ex.message}")
             throw ex
         }
@@ -42,6 +57,16 @@ class SykemeldingNLKafkaProducer(private val producer: KafkaProducer<String, INl
         try {
             producer.send(ProducerRecord(SYKEMELDING_NL_TOPIC, nlAvbrutt.orgnummer, kafkaMessage)).get()
         } catch (ex: Exception) {
+            when(source){
+                NlResponseSource.LPS -> {
+                    COUNT_FAILED_REVOKE_LINEMANAGER_FROM_EMPTY_FORM_BY_LPS.increment()
+                }
+                NlResponseSource.PERSONALLEDER -> {
+                    COUNT_FAILED_REVOKE_LINEMANAGER_FROM_EMPTY_FORM_BY_PERSONNEL_MANAGER.increment()
+                }
+                else -> {
+                }
+            }
             logger.error("Exception was thrown when attempting to send NlAvbrutt: ${ex.message}")
             throw ex
         }

81f80ccc1235f6fd25d7785ee95f7467554b5dbe
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2026-02-17 10:08:50 +0100

    Add logging

--- src/main/kotlin/no/nav/syfo/sykmelding/db/SendtSykmeldingDb.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/db/SendtSykmeldingDb.kt
@@ -140,10 +140,11 @@ class SykmeldingDb(
                 connection.prepareStatement(
                     """
                     SELECT sykmelding_id FROM sendt_sykmelding
-                    WHERE fnr = ? AND orgnummer = ?;
+                    WHERE fnr = ? AND orgnummer = ? 
                     """.trimIndent()
                 ).use { preparedStatement ->
                     map.forEach { (fnr, orgnummer) ->
+                        logger.info("Finding sykmeldingId for fnr: ${fnr.substring(0,4)} and orgnummer: $orgnummer")
                         preparedStatement.setString(1, fnr)
                         preparedStatement.setString(2, orgnummer)
                         preparedStatement.addBatch()

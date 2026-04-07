bf76541674662cf985f6d76036f9b782819af939
Christian Gregersen <71885781+cgregers@users.noreply.github.com>
2026-04-07 10:51:40 +0200

    Add grant for the sendt_sykmelding table

--- src/main/resources/db/migration/R__grant_analysis_privileges.sql
+++ src/main/resources/db/migration/R__grant_analysis_privileges.sql
@@ -1,2 +1,4 @@
 -- Grant SELECT privileges on all tables in the PUBLIC schema to the esyfo-analyse role
 GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "esyfo-analyse";
+
+-- Add grant for sendt_sykmelding table
